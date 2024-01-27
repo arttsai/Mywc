2d3b2c93b6de857cdaf29a2ebe1fa383828f4246
Art Tsai <arttsai@gmail.com>
2024-01-27 17:14:09 +0800

    drop DirectoryLinQ

--- DirectoryLinQ/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// use command line to get command arguments 
-// if no directory assigned, current directory is used
-// -v, --verbose: verbose mode, will print every file's filename and line count
-// -h, --help: print help message
-
-using CommandLine;
