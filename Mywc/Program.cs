22a75b22d3d3d495ce2287aa9bd9c2961e4d5e93
Art Tsai <arttsai@gmail.com>
2024-01-28 09:33:15 +0800

    without command line parser

--- Mywc/Program.cs
+++ Mywc/Program.cs
@@ -1,36 +1,72 @@
-﻿// mywc 
-//    -l, --line: 計算列數 (default) 
-//    -w, --word: 計算字數
-//    -c, --char: 計算字元數 
-//    -s, --silent: 不顯示詳細資訊, 僅列出總和
-//    -t, --type: 檔案類型 (附加名)
-//    -h, --help: 程式說明 (command line parser 會自動產生)
-//    path: 檔案或資料夾路徑
+﻿using System.Text;
 
-using System.Text.Json;
-using CommandLine;
-using Mywc;
-
-var argsStr = string.Join(' ', args);
-Console.WriteLine(argsStr);
-Console.WriteLine();
-
-var result = Parser.Default.ParseArguments<Option>(args)
-    .WithParsedAsync(RunAsync);
-return;
-
-async Task RunAsync(Option option)
+var dir = args[0];
+var option = new Option()
 {
-    //var jso = new JsonSerializerOptions() { WriteIndented = true };
-    //var json = JsonSerializer.Serialize(option, jso);
-    //Console.WriteLine(json);
+    CountingLine = true,
+    CountingWord = true,
+    CountingChar = true,
+    Types = new[] { "cs"}
+};
 
-    // 如果 option.Types 是 null, 將它設為包含 cs 的字串陣列
-    option.Types ??= new[] { "cs" };
+var (lines, words, chars) = await CountDirectoryAsync(option, dir);
+Console.WriteLine($"Total lines: {lines}");
+Console.WriteLine($"Total words: {words}");
+Console.WriteLine($"Total chars: {chars}");
 
-    var wcCounter = new WcCounter();
-    (int lines, int words, int chars) = await wcCounter.RunAsync(option); 
-    if (lines > 0) Console.WriteLine($"Total Lines: {lines}");
-    if (words > 0) Console.WriteLine($"Total Words: {words}");
-    if (chars > 0) Console.WriteLine($"Total Chars: {chars}");
+
+async Task<(int lines, int words, int chars)> CountDirectoryAsync(Option option, string dirPath)
+{
+    var (lines, words, chars) = (0, 0, 0);
+
+    var files = Directory.GetFiles(dirPath, "*.*", SearchOption.AllDirectories);
+        
+    for (var i = 0; i < files.Length; i++)
+    {
+        var file = files[i];
+        var ext = Path.GetExtension(file).TrimStart('.');
+        if (!option.Types!.Contains(ext)) continue;
+
+        var (fileLines, fileWrods, fileChars) = await CountFileAsync(option, file);
+        lines += fileLines;
+        words += fileWrods;
+        chars += fileChars;
+    }
+
+    return (lines, words, chars);   
+}
+
+async Task<(int lines, int words, int chars)> CountFileAsync(Option option, string filePath)
+{
+    var (lines, words, chars) = (0, 0, 0);
+
+    using var reader = new StreamReader(filePath);
+    while (await reader.ReadLineAsync() is { } line)
+    {
+        lines++;
+        if (option.CountingWord) words += line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+        if (option.CountingChar) chars += line.Length;
+    }
+
+    if (!option.Silent)
+    {
+        var sb = new StringBuilder();
+        sb.Append('[');
+        if (option.CountingLine) sb.Append(lines);
+        if (option.CountingWord) sb.Append(", ").Append(words);
+        if (option.CountingChar) sb.Append(", ").Append(chars);
+        sb.Append("] ").Append(filePath);
+        Console.WriteLine(sb.ToString());
+    }
+
+    return (lines, words, chars);
+}
+
+public class Option
+{
+    public bool CountingLine { get; set; }
+    public bool CountingWord { get; set; }
+    public bool CountingChar { get; set; }
+    public bool Silent { get; set; }
+    public IEnumerable<string>? Types { get; set; }
 }
